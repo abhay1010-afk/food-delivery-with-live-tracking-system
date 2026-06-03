ace18ab124e4e9b205e08d7bedda0a65580681de
abhay1010-afk <abhaytomar1010@gmail.com>
2026-06-03 16:27:50 +0530

    Update CORS origin for backend server

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const server=http.createServer(app)
 
 const io=new Server(server,{
    cors:{
-    origin:"https://food-delivery-with-live-tracking-system-1.onrender.com",
+    origin:"https://tomato-food-delivery-app-3hzl.onrender.com",
     credentials:true,
     methods:['POST','GET']
 }
@@ -31,7 +31,7 @@ app.set("io",io)
 
 const port=process.env.PORT || 5000
 app.use(cors({
-    origin:"https://food-delivery-with-live-tracking-system-1.onrender.com",
+    origin:"https://tomato-food-delivery-app-3hzl.onrender.com",
     credentials:true
 }))
 app.use(express.json())
