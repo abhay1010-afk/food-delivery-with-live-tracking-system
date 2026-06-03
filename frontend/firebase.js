668357f0877244451d9220aaecd6842a24e611d8
abhay1010-afk <abhaytomar1010@gmail.com>
2026-06-03 14:41:27 +0530

    new updated changes

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -6,15 +6,15 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey:import.meta.env.VITE_FIREBASE_APIKEY,
-  authDomain: "vingo-food-delivery.firebaseapp.com",
-  projectId: "vingo-food-delivery",
-  storageBucket: "vingo-food-delivery.firebasestorage.app",
-  messagingSenderId: "693314883513",
-  appId: "1:693314883513:web:4840c943ecf3fb61a33068"
+  apiKey: "AIzaSyAlYksZMupbZ7C9kRzbmZRFGPQsxrKSquw",
+  authDomain: "tomato-food.firebaseapp.com",
+  projectId: "tomato-food",
+  storageBucket: "tomato-food.firebasestorage.app",
+  messagingSenderId: "1047308204482",
+  appId: "1:1047308204482:web:888432c8e1caeca5fd3e5b"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth=getAuth(app)
-export {app,auth}
+const auth=getAuth(app);
+export {app,auth};
